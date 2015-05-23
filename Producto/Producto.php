2dfcd6a7bcc9c79492977cfbd7cb823eec099f49
Jose Slon <jslon0891@gmail.com>
2015-05-23 17:15:01 -0600

    BD
    Base de datos, Diagrama y codigo de producto

---
+++ Producto/Producto.php
@@ -0,0 +1,16 @@
+<?php
+if (isset($_POST['nombre']) && !empty($_POST['nombre']))
+{
+$conec = pg_connect("host=localhost  dbname=Punto_Venta user=postgres password=ECCIpsql2014 ") or die("problemas de coneccion " . pg_last_error());
+
+$query = "INSERT INTO producto(nombre,marca,unidad,cantidad,minimo,empresa) Values ('$_POST[nombre]' ,'$_POST[marca]' ,'$_POST[unidad]' ,'$_POST[cantidad]' ,'$_POST[minimo]','1')";
+pg_query($conec,$query) or die('La consulta fallo: ' . pg_last_error());
+
+
+echo"Exito";
+}
+else
+{
+echo"error";
+}
+?>
