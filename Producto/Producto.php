c6286e3303de2442dec2fb5aa0a46cfd2b8756c9
Jose Slon <jslon0891@gmail.com>
2015-05-24 13:06:29 -0600

    .

--- Producto/Producto.php
+++ Producto/Producto.php
@@ -1,7 +1,7 @@
 <?php
 if (isset($_POST['nombre']) && !empty($_POST['nombre']))
 {
-$conec = pg_connect("host=localhost  dbname=Punto_Venta user=postgres password=ECCIpsql2014 ") or die("problemas de coneccion " . pg_last_error());
+$conec = pg_connect("host=localhost  dbname=Punto_Venta user=postgres password=admin ") or die("problemas de coneccion " . pg_last_error());
 
 $query = "INSERT INTO producto(nombre,marca,unidad,cantidad,minimo,empresa) Values ('$_POST[nombre]' ,'$_POST[marca]' ,'$_POST[unidad]' ,'$_POST[cantidad]' ,'$_POST[minimo]','1')";
 pg_query($conec,$query) or die('La consulta fallo: ' . pg_last_error());
